284d17aae4d0d72d37e165425987d7514f25ef43
Santiago Toyos <ppikachu@gmail.com>
2023-09-08 15:50:27 -0300

    Update nuxt.config.ts
    vercel?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/image"],
-  runtimeConfig: {
+	devtools: { enabled: true },
+	modules: ["@nuxt/ui", "@nuxt/image"],
+	runtimeConfig: {
 		public: {
 			RapidAPIKey: process.env.RapidAPIKey,
 			RapidAPIHost: process.env.RapidAPIHost,
 		},
+	},
+	image: {
+		format: ["webp", "png"],
+		provider: "ipx",
+		ipx: {
+			// @ts-ignore
+			modifiers: {
+				quality: "80",
+				format: ["webp", "png"],
+			},
+		},
+	},
+	ui: {
+		icons: ["mdi"],
 	},
 })
